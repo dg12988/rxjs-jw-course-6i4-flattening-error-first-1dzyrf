abc25710a4da2fa5c3dc5e7c486607c02d0ca498
dg12988 <doug.goldberg@me.com>
2021-08-12 16:22:03 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { fromEvent } from "rxjs";
+import { EMPTY, fromEvent } from "rxjs";
 import { ajax } from "rxjs/ajax";
-import { concatMap, map } from "rxjs/operators";
+import { catchError, concatMap, map } from "rxjs/operators";
 
 const endpointInput: HTMLInputElement = document.querySelector('input#endpoint');
 const fetchButton = document.querySelector('button#fetch');
@@ -9,7 +9,10 @@ fromEvent(fetchButton, 'click').pipe(
   map(() => endpointInput.value),
   concatMap(value =>
     ajax(`https://random-data-api.com/api/${value}/random_${value}`)
-  )
-).subscribe(
-  value => console.log(value)
-);
+  ),
+  catchError(() => EMPTY)
+).subscribe({
+  next: value => console.log(value),
+  error: err => console.log('Error:', err),
+  complete: () => console.log('Completed')
+});
